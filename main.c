922e3200ab7be5fe8cfde318df30713811e9061a
PedroVargas812 <pedro.vargas@udla.edu.ec>
2024-12-13 08:36:18 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,39 +1,37 @@
 #include <stdio.h>
 #include "funciones.h"
 
-int main (int argc, char *argv[]) {
-
-    struct Libro libros[20];
+int main() {
+    struct libro libro[20];
+    int contador=0;
     int opc;
-    int i=0;
-    do{
-    opc = menu();
-    switch (opc)
-    {
-        case 1:
-            registrarLibros(libros, i);
-            break;
-        case 2:
-            mostrarLibros(libros);
-            break;
-        case 3:
-            int id;
-            printf("Ingrese el id del libro a buscar: ");
-            scanf("%d", &id);
-            buscarLibroId(libros, id);
-            i++;
-            break;
-        case 4:
-            char titulo[100];
-            printf("Ingrese el titulo del libro a buscar: ");
-            scanf("%s", titulo);
-            buscarLibroTitulo(libros, titulo);
-            break;
-        
 
-    }
-    }while(opc != 7);
+    do {
+        opc = menu();
 
+        switch (opc) {
+            case 1:
+                registrarLibro(libro, &contador);
+                break;
+            case 2:
+                buscarLibro(libro, contador);
+                break;
+            case 3:
+                mostrarLibros(libro, contador);
+                break;
+            case 4:
+                actualizarEstado(libro, contador);
+                break;
+            case 5:
+                eliminarLibro(libro, &contador);
+                break;
+            case 6:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida. Intente nuevamente.\n");
+        }
+    } while (opc != 6);
 
     return 0;
 }
